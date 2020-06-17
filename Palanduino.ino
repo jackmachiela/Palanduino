5b0a2cc37c57aab0a653a6128d305dfb11c7372e
Jack Machiela <jack@pobox.com>
2020-06-17 21:00:21 +1200

    Install of Github app on desktop, and first uploaded version. Local version was already up to v4, so that's what we're going with here. Tons of changes - not commenting them all.

--- Palanduino.ino
+++ Palanduino.ino
@@ -2,184 +2,297 @@
 //
 //           Written during Corona Lockdown 2020 by Jack Machiela <jack@pobox.com>
 //           Inspiration from /u/cakeanalytics ( https://github.com/BetterWayElectronics/esp8266-corona-tracking-i2c-oled )
+//           Loads of programming tips from my son Josh Machiela <howdidthishappen@hesmyson.itaughthimeverythingiknow.com>
 //
 //           Channel credits: Corona Virus data API provided by Javier Aviles https://github.com/javieraviles
 //           
 //  Components:
 // * 1x Arduino/ESP8266 NodeMCU
 // * 1x 1602A LCD Display (2x16 chars). I'm using a 16-pin one - 14 pin versions should work with minor modifications.
-// * 3x 10k Ohm Potentiometers
 // * 1x 360deg Rotary Encoder
 //
-//  The 1602A LCD Display:
-// * LCD(01) VSS      - MCU ground
-// * LCD(02) VCC/VDD  - Wiper Pot(1) - other two Pot(1) pins to MCU 5V(VV) & (unused)   - BRIGHTNESS
-// * LCD(03) VO       - Wiper Pot(2) - other two Pot(2) pins to MCU 5V(VV) & MCU GND    - CONTRAST
-// * LCD(04) RS       - MCU D2
-// * LCD(05) R/W      - MCU GND
-// * LCD(06) Enable   - MCU D3
-// * LCD(07) D0       - (unused)                      //  The Rotary Encoder:
-// * LCD(08) D1       - (unused)                      // * RE(1) CLK  - MCU D4
-// * LCD(09) D2       - (unused)                      // * RE(2) DT   - MCU D0
-// * LCD(10) D3       - (unused)                      // * RE(3) SW   - MCU D1
-// * LCD(11) D4       - MCU D5                        // * RE(4) +    - MCU 3V
-// * LCD(12) D5       - MCU D6                        // * RE(5) GND  - MCU GND
-// * LCD(13) D6       - MCU D7
-// * LCD(14) D7       - MCU D8
-// * LCD(15) A        - Wiper Pot(3) - other Pot(3) pins to MCU 5V(VV) & (unused)   - BACKLIGHT
-// * LCD(16) K        - MCU GND
+//  The 1602A LCD Display hooks into the IIC/I2C 1602A 2004 Adapter Plate, which has four pins:
+// * IIC(01) GND  - MCU ground
+// * IIC(02) VCC  - MCU 5V(VV)
+// * IIC(03) VO   - MCU D3
+// * IIC(04) RS   - MCU D4
+//  NB - some displays come with the IIC/I2C module already connected - if not, get one and solder it on. The screen is so much easier to deal with on the I2C interface.
 //
-// NB - The three Pots may need to be experimented with so they turn the right way (CCW = lower, CW = higher intensities).
-//      If necessary, swap +(/-) terminals to other (non-swiper) side.
+//  The Rotary Encoder:
+// * RE(5) GND  - MCU GND
+// * RE(4) +    - MCU 5V(VV)
+// * RE(3)                                   (SW   - MCU D2)
+// * RE(2) DT   - MCU D1
+// * RE(1) CLK  - MCU D0
 //
 //  The NodeMCU (remaining pins), if using external PSU:
-// * MCU VV    - +5v
-// * MCU G     - GND
+// * MCU VV    - PSU +5v
+// * MCU G     - PSU GND
 //
-// NB - NEVER connect external PSU at the same time as powering through USB connector, as nuclear detonation may occur.
-//
-
-
 
 // Libraries to include:
-#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <LiquidCrystal.h>
+#include <WiFiManager.h>      // Using the Arduino Library Manager, install "WifiManager by tzapu" - https://github.com/tzapu/WiFiManager
+
+#include <ezTime.h>           // lib at https://github.com/ropg/ezTime, docs at https://awesomeopensource.com/project/ropg/ezTime
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <ArduinoJson.h>
+#include <MD_REncoder.h>      // Majic Designs Rotary Encoder Library    https://github.com/MajicDesigns/MD_REncoder
 
-/////////////////////////  WiFi Details - remove before publishing online
-
-#define R_SSID       "ABCDEF"       // Wifi network name
-#define PASS         "PASSWD"          // Wifi Password
-
-const char* ssid     = R_SSID;
 
 ///////////////////////// Globals
 
-// Create an ESP8266 WiFiClient class to connect to the various servers.
-WiFiClient client;
 
-// initialise the library with the numbers of the interface pins
-const int RS = D2, EN = D3, d4 = D5, d5 = D6, d6 = D7, d7 = D8;
-LiquidCrystal lcd(RS, EN, d4, d5, d6, d7);
+// Define some wibbley wobbley timey wimey stuff
+Timezone localTimeZone;
+String TimeZoneDB = "Pacific/Auckland";      // See full list here: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones 
+String localNtpServer = "nz.pool.ntp.org";           // See full list here: http://support.ntp.org/bin/view/Servers/WebHome
+bool showColon = true;
+
+
+//  setDebug(INFO);  // Outputs NTP updates to serial monitor
+//  setServer(localNtpServer);
+//  Timezone timeZone;
+//  setInterval(30);    //ntp polling interval     - temp, remove when done
+//  timeZone.setLocation(localTimeZone);
+//  waitForSync();      //Make sure to start with accurate time
+
+
+// initialise the LCD instance
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 //Define strings that will be displayed to the LCD screen
-String lineOne;
-String lineTwo;
+  String lineOne;
+  String lineTwo;
+  //Define two more strings to see if the message has changed since last screen update
+  String prevLineOne="";
+  String prevLineTwo="";
+
+
+//Define some vars for the Rotary Encoder (Channel Selector dial)
+  MD_REncoder rotEnc = MD_REncoder(D0, D1);      //RotEnc's DR, CLK pins
+  unsigned long long lastRotEncAction = 0;                       // Contains millis since last Rotary Encoder was clicked or rotated
+
 
 // Some vars to limit the rate of server interrogations (throttle - don't strangle the internet!)
-int interval;                                  // each module will have its own interval
-unsigned long long lastCheckedMillis;
-unsigned long long currentMillis;
+  int interval=1;                                  // each module will have its own interval, but start with 1 second
+  unsigned long long lastCheckedMillis;
+  unsigned long long currentMillis;
+
+// And some Channel stuff
+  int totalChannels=4;             // Total number of available channels
+  int currentChannel=1;            // Start on Channel 1
+
+
+
+
+
+
+
+
 
 void setup() {                    ///////////////////////////////////////////////////////////////////////////////////////////////// VOID SETUP
-  Serial.begin(115200);
-  delay(10);
+  Serial.begin(74880);
+  delay(10);      // let the serial connection calm down
+
 
   // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
+  Wire.begin(2,0);      // set up a IIC instance
+  lcd.init();           // initialise the LCD display
+  lcd.backlight();      // switch on the backlight 
 
   // Initial welcome message
   // Restrict to 16 characters
-  //       [ xxxxxxxxxxxxxxxx ]
-  lineOne = "   Palanduino   ";
-  lineTwo = "======== v1.0 ==";
+  //         [ xxxxxxxxxxxxxxxx ]
+    lineOne = "   Palanduino   ";
+    lineTwo = "======== v4.0 ==";
+                 // v1.0 was initial basic code, one channel (NZ Covid-19 data update)
+                 // v1.1 changed the main structure and smoothed it out, ready for multi-channel
+                 // v1.2 - Add Rotary Encoder
+                 // v2.0 - Changed LCD to use IIC/I2C protocol, saving 3x POTs, 16x Jumper wires, 4x NodeMCU pins
+                 // v3.0 - Changed hardcoded WiFi details to WiFi Manager's on-the-fly configuration (safer!)
+                 //        Also added clock and NTP libraries
+                 // v4.0 - changed NTP library to EZTime
+                 //
+                 
 
-  // Print the data to the screen
-  updateLCD();
+  // Print the title to the screen
+    updateLCD();
+    delay(1500); // and give it a couple of seconds
+  
 
   // Initialise the internet link (login to Wi-Fi)
   init_wifi();
+  
+  setDebug(INFO);   // Outputs NTP updates to serial monitor
+  setServer(localNtpServer);
+  setInterval(30);    //ntp polling interval     - temp, remove when done
+  waitForSync();      //Make sure to start with accurate time
+//  Timezone localTimeZone;
+  localTimeZone.setLocation(TimeZoneDB);
+///////////////////////////////////  localTimeZone.setDefault();   // ??????
 
-  //Make sure the first module gets to check something straight away
- // lastCheckedMillis = (-100000);   // To ensure the first module gets to GET DATA straight away
+//  Timezone timeZone;
+//  timeZone.setLocation(localTimeZone);
+
+
+  //checkNtpStatus();         // temp, remove when done
+  Serial.println("[X] setup] UTC: " + UTC.dateTime());
+  Serial.println("[X] Local time: " + localTimeZone.dateTime());
+
+
+  // Initialise the Rotary Encoder
+    rotEnc.begin();
 
 }
-
 
 void loop() {                    ///////////////////////////////////////////////////////////////////////////////////////////////// VOID LOOP
+//  Timezone localTimeZone;
+//  localTimeZone.setLocation(TimeZoneDB);
 
-  // Print the data to the screen
+//  Serial.println("[Y] Local time: " + localTimeZone.dateTime());
+
+  
+  // lastRotEncAction
+  
+  uint8_t rotEncReading = rotEnc.read();
+
+    if (rotEncReading)
+  {
+
+    currentChannel=(currentChannel + (rotEncReading == DIR_CW ? 1 : -1));   // Add or subtract 1 to the current channel
+    if (currentChannel > totalChannels) currentChannel = 1;                 // If it goes over the maximum available channel, reset to 1
+    if (currentChannel < 1) currentChannel = totalChannels;                 // If it goes under the lowest available channel, reset to highest
+    interval = 0;                               //Channel changed - reset straight away
+    lastRotEncAction = millis();                // Set last action to current time
+    
+    Serial.print("    currentChannel = ");
+    Serial.println(currentChannel);
+
+  }
+
+  if ((lastRotEncAction + 1800) >= millis())       //if less than 3 seconds have passed since Rotatry encoder was used, display channel titles only
+    {
+
+      lineOne = "Select Channel: ";
+      lineTwo = "                ";
+      
+      if (currentChannel == 1) lineTwo = "1: Time & Date  ";                  //
+      if (currentChannel == 2) lineTwo = "2: NZ Covid-19  ";                  //Get updated New Zealand Covid-19 data
+      if (currentChannel == 3) lineTwo = "3: Channel Three";                  //
+      if (currentChannel == 4) lineTwo = "4: Channel Four ";                  //
+      
+      updateLCD();
+
+    }
+  else
+    {
+      // Check if interval for this channel has been reached
+      currentMillis = millis();
+      if ((lastCheckedMillis==0) || ((currentMillis - lastCheckedMillis) > (interval*1000)))  {
+  
+      if (currentChannel == 1) {                           //timeAndDate();                       //
+        interval = 1;         // (in seconds) How often should I check this website for updates after the initial check
+        // Initialise the SNTP time synchro
+        //timeZone.setLocation(localTimeZone);
+        Serial.println("A:Local time: " + localTimeZone.dateTime());
+        lineOne = localTimeZone.dateTime("  h:i:s A   ");
+        lineTwo = localTimeZone.dateTime("D d M, Y");
+        Serial.println("B:Local time: " + localTimeZone.dateTime());
+      }
+      if (currentChannel == 2) channelNZCovid19();                  //Get updated New Zealand Covid-19 data
+      if (currentChannel == 3) channelThree();                      //
+      if (currentChannel == 4) channelFour();                       //
+
+      lastCheckedMillis=millis();
+    
+    }
+  
+  }
+
+
   updateLCD();
-
-  //First channel - get updated New Zealand Covid-19 data
-  channel_covid19();
+  events();         // Check if any scheduled ntp events
 }
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// INITs
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// Functions
+
 
 void init_wifi() {
   // Connect to WiFi access point.
-  Serial.println(); Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(R_SSID);
-
-  lineOne = "LAN:";  lineOne += ssid;
-  lineTwo = ".";
-  
-  WiFi.begin(R_SSID, PASS);
-  int counter=10;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-
+    lineOne = "Connecting to   ";
+    lineTwo = "    LAN via WiFi";
     updateLCD();
-    lineTwo += ".";
-    
-    counter--;
-    if(counter==0){
-      // reset me
-        lineOne = "LAN login failed";
-        lineTwo = "Resetting...";
-        updateLCD();
-        delay(2000);
-        ESP.reset();
-    }
-  }
-  Serial.println();
 
-  Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  // Start WiFi
+    WiFiManager wifiManager;
+    wifiManager.autoConnect("Palanduino_Install");        // <----------- Create Wifi LAN to install with, if not previously initialised
 
-  lineOne = "LAN: ";
-  lineOne += ssid;
-
-  lineTwo = "IP: ";
-  lineTwo += WiFi.localIP().toString();
-
-  updateLCD();
+    delay(1000);
 
 }
 
+
+
+
+
 void updateLCD() {
-  // Print a message to the LCD.
-  lcd.clear();
-  lcd.setCursor(0, 0); lcd.print(lineOne);
-  lcd.setCursor(0, 1); lcd.print(lineTwo);
+//Serial.println("TEMP - lineOne" + lineOne);
+//Serial.println("TEMP - length" + lineOne.length());
 
-  Serial.println("Sent to LCD : ---------------------------------------------------");
-  Serial.println("               + -  -  -  -  -  - +");
-  Serial.print(  "                 "); Serial.println(lineOne);
-  Serial.print(  "                 "); Serial.println(lineTwo);
-  Serial.println("               + -  -  -  -  -  - +");
+//  while ( lineOne.length() < 16 ) {                // crashes the arduino for some reason. Needs work.
+//    lineOne = lineOne + " ";
+//  }
+//  while ( lineTwo.length() < 16 ) {
+//    lineOne=lineTwo+" ";
+//  }
+  
+  // Check if the data has been updated, otherwise don't bother updating the display
+  if ((lineOne != prevLineOne) || (lineTwo != prevLineTwo)) {
 
-  delay(2000);
+    // Print a message to the LCD.
+//    lcd.clear();
+    lcd.setCursor(0, 0); lcd.print(lineOne);
+    lcd.setCursor(0, 1); lcd.print(lineTwo);
+
+    // also print the message over the serial link (if arduino is connected via USB)
+    Serial.println("Sent to LCD : ---------------------------------------------------");
+    Serial.println("               + -  -  -  -  -  - +");
+    Serial.print(  "                 "); Serial.println(lineOne);
+    Serial.print(  "                 "); Serial.println(lineTwo);
+    Serial.println("               + -  -  -  -  -  - +");
+
+    // Set the previous lines as the current ones
+    prevLineOne = lineOne;
+    prevLineTwo = lineTwo;
+}
+  
 
 }
 
                     //////////////////////////////////////////////////////////////////////////////////////////////////////////// CHANNELS
 
 
-void channel_covid19() {
-  interval = 30;         // (in seconds)
-  currentMillis = millis();
+void timeAndDate() {
 
-  if ((lastCheckedMillis==0) || ((currentMillis - lastCheckedMillis) > (interval*1000)))  {
+  
+}
 
-    lineOne = "NZ Covid-19";
-    lineTwo = "Updating...";
+
+
+
+
+void channelNZCovid19() {
+  interval = 300;         // (in seconds) How often should I check this website for updates after the initial check
+
+    lineOne = "NZ Covid-19     ";
+    lineTwo = "Updating...     ";
     updateLCD();
+    
+    delay(1000);
 
     HTTPClient http;
     //GET directly from the URL (Dont use HTTPS) Modify the JSON Value as required!
@@ -209,13 +322,33 @@ void channel_covid19() {
         long totalTests = doc["totalTests"];
         int testsPerOneMillion = doc["testsPerOneMillion"];
 
-      lineOne = "Cov-19 Sick:" + String(cases);
-      lineTwo = "Dead:" + String(deaths) + " Ok:" + String(recovered);
+      lineOne = "Cov-19 - Sick:" + String(active) + " ";
+      lineTwo = "Dead:" + String(deaths) + " Ok:" + String(recovered)+ " ";
 
-      lastCheckedMillis=millis();
+//      lastCheckedMillis=millis();
+    }
+    else
+    {
+      lineOne = "Cov-19 - Sick:??";
+      lineTwo = "Dead:?? Ok:???? ";
+
     }
     http.end(); 
 
-  }
+//  }
 
+}
+
+void channelThree() {
+      interval = 1;         // (in seconds) How often should I check this website for updates after the initial check
+
+      lineOne = "Channel 3       ";
+      lineTwo = "Millis:" + String(millis()) + "     ";
+}
+
+void channelFour() {
+      interval = 1;         // (in seconds) How often should I check this website for updates after the initial check
+
+      lineOne = "Channel 4       ";
+      lineTwo = "Millis:" + String(millis()) + "     ";
 }
